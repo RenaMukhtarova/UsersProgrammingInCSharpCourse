dd244c392bec845f4f3949c6a52e3eb2d0ceeda7
Rana Hasanova <rena.muxtarovaa@gmail.com>
2023-10-01 10:05:19 +0400

    first commit

---
+++ CSharpCourse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main()
+        {
+            //task2
+
+        }
+        static void Main3()
+        {  //task3
+            double a= int.Parse(Console.ReadLine());
+            double b= int.Parse(Console.ReadLine());
+            double result = a / b;
+            Console.WriteLine(result);
+
+
+        }
+        static void Main2()
+        {
+            //task1
+            int a =int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+
+            bool result=a == b?true:false;
+            Console.WriteLine(result);
+        }
+    }
+
+}
